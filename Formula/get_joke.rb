6dbe24933d06b0903488d36a6aaf02cd352e1d79
samkahchiin <kcsam@siliconjungles.com>
2020-11-08 17:47:31 +0800

    :recycle: Rename
    Signed-off-by: samkahchiin <kcsam@siliconjungles.com>

--- Formula/get_joke.rb
+++ Formula/get_joke.rb
@@ -1,9 +1,9 @@
 class GetJoke < Formula
     desc "random joke about Chuck Norris"
-    homepage "https://github.com/samkahchiin/homebrew-random_joke"
+    homepage "https://github.com/samkahchiin/homebrew-get_joke"
     version "0.1"
 
-    url "https://github.com/samkahchiin/homebrew-random_joke/archive/main.zip", :using => :curl
+    url "https://github.com/samkahchiin/homebrew-get_joke/archive/main.zip", :using => :curl
 
     def install
         bin.install "bin/get_joke"
